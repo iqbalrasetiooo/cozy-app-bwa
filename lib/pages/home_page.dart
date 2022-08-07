100598ac9859f916a34e79ab9234834092342d7c
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-08-07 20:39:39 +0700

    add new updated line to homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:find_house_app/model/city.dart';
+import 'package:find_house_app/model/space.dart';
+import 'package:find_house_app/model/tips.dart';
+import 'package:find_house_app/widgets/bottom_navbar_item.dart';
 import 'package:find_house_app/widgets/city_card.dart';
 import 'package:find_house_app/theme.dart';
+import 'package:find_house_app/widgets/space_card.dart';
+import 'package:find_house_app/widgets/tips_card.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,26 +15,39 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: edge, horizontal: edge),
-          child: ListView(
-            children: [
-              Text(
+        bottom: false,
+        child: ListView(
+          children: [
+            const SizedBox(height: 30),
+            //NOTE: TITLE/HEADER
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Text(
                 'Explore Now',
                 style: blackTextStyle.copyWith(fontSize: 24),
               ),
-              const SizedBox(height: 2),
-              Text(
+            ),
+            const SizedBox(height: 2),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Text(
                 'Mencari kosan yang cozy',
                 style: greyTextStyle.copyWith(fontSize: 16),
               ),
-              const SizedBox(height: 30),
-              Text(
+            ),
+            const SizedBox(height: 30),
+            //NOTE: POPULAR CITIES
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Text(
                 'Popular Cities',
                 style: regularTextStyle.copyWith(fontSize: 16),
               ),
-              const SizedBox(height: 16),
-              Container(
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Container(
                 height: 150,
                 child: ListView(
                   scrollDirection: Axis.horizontal,
@@ -60,9 +78,126 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            //NOTE: RECOMMENDED SPACES
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Text(
+                'Recommended Space',
+                style: regularTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                children: [
+                  SpaceCard(
+                    space: Space(
+                        id: 1,
+                        name: 'Kuretakeso Hott',
+                        imageUrl: 'assets/space1.png',
+                        price: 52,
+                        city: 'Bandung',
+                        country: 'Germany',
+                        rating: 4),
+                  ),
+                  const SizedBox(height: 30),
+                  SpaceCard(
+                    space: Space(
+                        id: 2,
+                        name: 'Roemah Nenek',
+                        imageUrl: 'assets/space2.png',
+                        price: 11,
+                        city: 'Seattle',
+                        country: 'Bogor',
+                        rating: 5),
+                  ),
+                  const SizedBox(height: 30),
+                  SpaceCard(
+                    space: Space(
+                        id: 3,
+                        name: 'Darrling How',
+                        imageUrl: 'assets/space3.png',
+                        price: 20,
+                        city: 'Jakarta',
+                        country: 'Indonesia',
+                        rating: 3),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            // NOTE: TIPS & GUIDANCE
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Text(
+                'Tips & Guidance',
+                style: regularTextStyle.copyWith(fontSize: 16),
+              ),
+            ),
+            const SizedBox(height: 18),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: edge),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TipsCard(
+                    tips: Tips(
+                        id: 1,
+                        title: 'City Guidelines',
+                        imageUrl: 'assets/tips1.svg',
+                        updatedAt: '20 Apr'),
+                  ),
+                  const SizedBox(height: 20),
+                  TipsCard(
+                    tips: Tips(
+                        id: 2,
+                        title: 'Jakarta Fairship',
+                        imageUrl: 'assets/tips2.svg',
+                        updatedAt: '11 Dec'),
+                  ),
+                  const SizedBox(height: 30),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 65,
+        width: MediaQuery.of(context).size.width - (2 * edge),
+        margin: EdgeInsets.symmetric(horizontal: edge),
+        decoration: BoxDecoration(
+          color: const Color(0xfff6f7f8),
+          borderRadius: BorderRadius.circular(23),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_home_solid_active.svg',
+              isActive: true,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_mail_solid.svg',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_card_solid.svg',
+              isActive: false,
+            ),
+            BottomNavbarItem(
+              imageUrl: 'assets/Icon_love_solid.svg',
+              isActive: false,
+            ),
+          ],
         ),
       ),
     );
