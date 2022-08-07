b985cfc3c33c810a8c1d4e32cb9d1cb1bb34d9fc
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-08-07 20:41:07 +0700

    add new space_card widget

---
+++ lib/widgets/space_card.dart
@@ -0,0 +1,79 @@
+import 'package:find_house_app/model/space.dart';
+import 'package:find_house_app/theme.dart';
+import 'package:flutter/material.dart';
+
+class SpaceCard extends StatelessWidget {
+  final Space space;
+  const SpaceCard({Key? key, required this.space}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(18),
+          child: Container(
+            width: 130,
+            height: 110,
+            child: Stack(
+              children: [
+                Image.asset(space.imageUrl),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 70,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      color: purpleColor,
+                      borderRadius:
+                          BorderRadius.only(bottomLeft: Radius.circular(36)),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset('assets/Icon_star_solid.png',
+                            height: 22, width: 22),
+                        Text(
+                          '${space.rating}/5',
+                          style: whiteTextStyle.copyWith(fontSize: 13),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(width: 20),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              space.name,
+              style: blackTextStyle.copyWith(fontSize: 18),
+            ),
+            const SizedBox(height: 2),
+            Text.rich(
+              TextSpan(
+                text: '\$${space.price} ',
+                style: purpleTextStyle.copyWith(fontSize: 16),
+                children: [
+                  TextSpan(
+                      text: '/ Month',
+                      style: greyTextStyle.copyWith(fontSize: 16)),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              '${space.city}, ${space.country}',
+              style: greyTextStyle,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
