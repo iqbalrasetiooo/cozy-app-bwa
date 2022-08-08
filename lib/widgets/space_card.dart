209ff1694a080b99f45f9ac281355f5d332c46c4
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-08-08 17:11:48 +0700

    add new update

--- lib/widgets/space_card.dart
+++ lib/widgets/space_card.dart
@@ -1,4 +1,5 @@
 import 'package:find_house_app/model/space.dart';
+import 'package:find_house_app/pages/detail_page.dart';
 import 'package:find_house_app/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -8,72 +9,82 @@ class SpaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Container(
-            width: 130,
-            height: 110,
-            child: Stack(
-              children: [
-                Image.asset(space.imageUrl),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 70,
-                    height: 30,
-                    decoration: const BoxDecoration(
-                      color: purpleColor,
-                      borderRadius:
-                          BorderRadius.only(bottomLeft: Radius.circular(36)),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Image.asset('assets/Icon_star_solid.png',
-                            height: 22, width: 22),
-                        Text(
-                          '${space.rating}/5',
-                          style: whiteTextStyle.copyWith(fontSize: 13),
-                        )
-                      ],
-                    ),
-                  ),
-                )
-              ],
-            ),
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const DetailPage(),
           ),
-        ),
-        const SizedBox(width: 20),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              space.name,
-              style: blackTextStyle.copyWith(fontSize: 18),
-            ),
-            const SizedBox(height: 2),
-            Text.rich(
-              TextSpan(
-                text: '\$${space.price} ',
-                style: purpleTextStyle.copyWith(fontSize: 16),
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(
                 children: [
-                  TextSpan(
-                      text: '/ Month',
-                      style: greyTextStyle.copyWith(fontSize: 16)),
+                  Image.asset(space.imageUrl),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: const BoxDecoration(
+                        color: purpleColor,
+                        borderRadius:
+                            BorderRadius.only(bottomLeft: Radius.circular(36)),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/Icon_star_solid.png',
+                              height: 22, width: 22),
+                          Text(
+                            '${space.rating}/5',
+                            style: whiteTextStyle.copyWith(fontSize: 13),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
                 ],
               ),
             ),
-            const SizedBox(height: 16),
-            Text(
-              '${space.city}, ${space.country}',
-              style: greyTextStyle,
-            )
-          ],
-        )
-      ],
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                space.name,
+                style: blackTextStyle.copyWith(fontSize: 18),
+              ),
+              const SizedBox(height: 2),
+              Text.rich(
+                TextSpan(
+                  text: '\$${space.price} ',
+                  style: purpleTextStyle.copyWith(fontSize: 16),
+                  children: [
+                    TextSpan(
+                        text: '/ Month',
+                        style: greyTextStyle.copyWith(fontSize: 16)),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Text(
+                '${space.city}, ${space.country}',
+                style: greyTextStyle,
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
