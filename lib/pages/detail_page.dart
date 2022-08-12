25996fbdd11dfe2cd06af7356be21a27ae1eb166
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-08-12 18:41:02 +0700

    add new update per 12-08-2022

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -3,13 +3,19 @@ import 'package:find_house_app/theme.dart';
 import 'package:find_house_app/widgets/facility_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
 
 class DetailPage extends StatelessWidget {
   const DetailPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Future<void> launchUrl(Uri url) async {
+    //   await UrlLauncher.canLaunchUrl(url) ? await launchUrl(url) : throw (url);
+    // }
+
     return Scaffold(
+      backgroundColor: whiteColor,
       body: SafeArea(
         bottom: false,
         child: Stack(
@@ -19,27 +25,6 @@ class DetailPage extends StatelessWidget {
               height: 350,
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 30, horizontal: edge),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: SvgPicture.asset(
-                      'assets/btn_back.svg',
-                      width: 40,
-                    ),
-                  ),
-                  SvgPicture.asset(
-                    'assets/btn_wishlist.svg',
-                    width: 40,
-                  ),
-                ],
-              ),
             ),
             ListView(
               children: [
@@ -200,6 +185,7 @@ class DetailPage extends StatelessWidget {
                         ),
                       ),
                       const SizedBox(height: 30),
+                      //NOTE: LOCATION
                       Padding(
                         padding: EdgeInsets.only(left: edge),
                         child: Text(
@@ -209,37 +195,64 @@ class DetailPage extends StatelessWidget {
                       ),
                       const SizedBox(height: 6),
                       Padding(
-                        padding: EdgeInsets.only(left: edge, right: edge),
+                        padding: EdgeInsets.symmetric(horizontal: edge),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text('Jln. Kappan Sukses No. 20\nPalembang',
-                                style: greyTextStyle.copyWith(fontSize: 14)),
-                            SvgPicture.asset('assets/btn_location.svg'),
+                            Text(
+                              'Jln. Kappan Sukses No. 20\nPalembang',
+                              style: greyTextStyle.copyWith(fontSize: 14),
+                            ),
+                            InkWell(
+                              onTap: () async {
+                                final maps = Uri.parse(
+                                    'https://www.google.com/maps/place/Alun+alun+Haurgeulis/@-6.4576568,107.9405106,15z/data=!4m5!3m4!1s0x2e693730da4e02a3:0x1019e5dfd209eada!8m2!3d-6.4596078!4d107.9434459');
+                                if (await UrlLauncher.canLaunchUrl(maps)) {
+                                  UrlLauncher.launchUrl(maps);
+                                } else {
+                                  print("can't launch $maps");
+                                }
+                              },
+                              child:
+                                  SvgPicture.asset('assets/btn_location.svg'),
+                            ),
                           ],
                         ),
                       ),
                       const SizedBox(height: 40),
                       Padding(
-                        padding: EdgeInsets.only(left: edge, right: edge),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(17),
-                          child: Container(
-                            height: 50,
-                            width: MediaQuery.of(context).size.width,
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                backgroundColor:
-                                    MaterialStateProperty.all<Color>(
-                                  purpleColor,
+                        padding: EdgeInsets.symmetric(horizontal: edge),
+                        child: Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width - (2 * edge),
+                          child: ElevatedButton(
+                            onPressed: () async {
+                              final phone = Uri.parse('tel:081324626295');
+                              if (await UrlLauncher.canLaunchUrl(phone)) {
+                                UrlLauncher.launchUrl(phone);
+                              } else {
+                                print("can't launch $phone");
+                              }
+                            },
+                            // onPressed: () {
+                            //   UrlLauncher.launchUrl(
+                            //       Uri.parse('tel:081324626295'));
+                            // },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(17),
                                 ),
                               ),
-                              child: Text(
-                                'Book Now',
-                                style: whiteTextStyle.copyWith(
-                                  fontSize: 18,
-                                ),
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                purpleColor,
+                              ),
+                            ),
+                            child: Text(
+                              'Book Now',
+                              style: whiteTextStyle.copyWith(
+                                fontSize: 18,
                               ),
                             ),
                           ),
@@ -250,6 +263,27 @@ class DetailPage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: edge),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: SvgPicture.asset(
+                      'assets/btn_back.svg',
+                      width: 40,
+                    ),
+                  ),
+                  SvgPicture.asset(
+                    'assets/btn_wishlist.svg',
+                    width: 40,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
