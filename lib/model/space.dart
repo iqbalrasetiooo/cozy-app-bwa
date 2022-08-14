4075ac644a46300c59e9cdf4cda781601e7ffbe4
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-08-14 09:25:50 +0700

    add space_fromJson

--- lib/model/space.dart
+++ lib/model/space.dart
@@ -1,18 +1,50 @@
 class Space {
-  int id;
-  String name;
-  String imageUrl;
-  int price;
-  String city;
-  String country;
-  int rating;
+  late int id;
+  late String name;
+  late String imageUrl;
+  late int price;
+  late String city;
+  late String country;
+  late int rating;
+  late String address;
+  late String phone;
+  late String mapUrl;
+  late List photos;
+  late int numberOfKitchens;
+  late int numberOfBedrooms;
+  late int numberOfCupboards;
 
-  Space(
-      {required this.id,
-      required this.name,
-      required this.imageUrl,
-      required this.price,
-      required this.city,
-      required this.country,
-      required this.rating});
+  Space({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+    required this.city,
+    required this.country,
+    required this.rating,
+    required this.address,
+    required this.phone,
+    required this.mapUrl,
+    required this.photos,
+    required this.numberOfCupboards,
+    required this.numberOfBedrooms,
+    required this.numberOfKitchens,
+  });
+
+  Space.fromJson(json) {
+    id = json['id'];
+    name = json['name'];
+    city = json['city'];
+    country = json['country'];
+    imageUrl = json['image_url'];
+    rating = json['rating'];
+    price = json['price'];
+    address = json['address'];
+    phone = json['phone'];
+    mapUrl = json['map_url'];
+    photos = json['photos'];
+    numberOfKitchens = json['number_of_kitchens'];
+    numberOfCupboards = json['number_of_cupboards'];
+    numberOfBedrooms = json['number_of_bedrooms'];
+  }
 }
