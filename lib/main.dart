a4538297d046941dc4b466ad121c0580e6a6207c
Iqbal Rasetio <rasetioiqbal@gmail.com>
2022-08-14 09:28:51 +0700

    wrapping main with changeNotifierProvider widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:find_house_app/pages/splash_page.dart';
+import 'package:find_house_app/providers/space_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +13,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashPage(),
+    return ChangeNotifierProvider(
+      create: (context) => SpaceProvider(),
+      child: const MaterialApp(
+        home: SplashPage(),
+      ),
     );
   }
 }
